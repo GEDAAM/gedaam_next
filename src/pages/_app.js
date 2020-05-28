fd055a508299380d85d91d7dd62fad07737cb9dd
Rafael Wendel <rafawendel2010@gmail.com>
2020-05-28 09:39:10 -0300

    testing

---
+++ src/pages/_app.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+// import App from 'next/app';
+import Head from 'next/head';
+
+import '../assets/styles/index.css';
+
+function GedaamApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>{/* ADD SEO HERE */}</Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+GedaamApp.propTypes = {
+  Component: PropTypes.element.isRequired,
+  pageProps: PropTypes.arrayOf(PropTypes.any).isRequired
+};
+
+export default GedaamApp;
